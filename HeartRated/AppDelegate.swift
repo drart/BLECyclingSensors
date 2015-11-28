da762d0676a30464a8d67b51d211ba4c898f425e
Adam Tindale <adam@adamtindale.com>
2015-11-28 15:08:02 -0500

    Initial Commit

---
+++ HeartRated/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  HeartRated
+//
+//  Created by Adam Tindale on 2015-11-28.
+//  Copyright (c) 2015 Adam Tindale. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ HeartRated/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  HeartRated
+//
+//  Created by Adam Tindale on 2015-11-28.
+//  Copyright (c) 2015 Adam Tindale. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
