8468f644595cb53a5c72162f5d2b6e7d8a6963ec
Adam Tindale <adam@adamtindale.com>
2015-11-29 14:50:05 -0500

    initial commit

--- HeartRated/ViewController.swift
+++ HeartRated/ViewController.swift
@@ -7,13 +7,24 @@
 //
 
 import Cocoa
+import CoreBluetooth
 
-class ViewController: NSViewController {
+class ViewController: NSViewController, CBCentralManagerDelegate, CBPeripheralDelegate {
 
+    var centralManager : CBCentralManager!
+    var powerMeter : CBPeripheral!
+    var heartRateMonitor : CBPeripheral!
+    var speedSensor : CBPeripheral!
+    var cadenceSensor : CBPeripheral!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        centralManager = CBCentralManager(delegate: self, queue: nil)
+        
+        //[CBUUID(string:"180D")]
+        //centralManager.scanForPeripheralsWithServices(nil, options: nil)
+        
     }
 
     override var representedObject: AnyObject? {
@@ -22,6 +33,52 @@ class ViewController: NSViewController {
         }
     }
 
+    
+    /*-----CentralManagerDelegate-----*/
+    
+    func centralManagerDidUpdateState(central: CBCentralManager) {
+        if central.state == CBCentralManagerState.PoweredOn{
+            centralManager.scanForPeripheralsWithServices(nil, options: nil)
+            println("scanning");
+        }else{
+            print("ble is off")
+        }
+        
+    }
 
+    func centralManager(central: CBCentralManager!, didDiscoverPeripheral peripheral: CBPeripheral!, advertisementData: [NSObject : AnyObject]!, RSSI: NSNumber!) {
+        if peripheral.name != nil{
+            println( peripheral.name )
+        }else{
+            println("wtf")
+        }
+        
+        //[central .connectPeripheral(peripheral, options: nil)]
+    }
+    
+    func centralManager(central: CBCentralManager!, didConnectPeripheral peripheral: CBPeripheral!) {
+        println("did connect peripheral " + peripheral.name)
+        peripheral.discoverServices(nil)
+    }
+ 
+    
+    /*-----PeripheralDelegate-----*/
+
+    func peripheral(peripheral: CBPeripheral!, didDiscoverServices error: NSError!) {
+        for  service in peripheral.services!{
+            let thisService = service as! CBService
+            
+            
+            //if ([thisService.UUID isEqual:[CBUUID UUIDWithString:@"180D"]])
+            if service.UUIDString == "180D"
+            {
+                println("heart rate service found")
+                //[peripheral discoverCharacteristics:nil forService:aService];
+            }
+            
+            
+        }
+    }
+    
 }
 
